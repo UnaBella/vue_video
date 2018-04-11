2c7ed61e9a8eba64b46c11ce71628f5b6cd0e278
Una Bella <369152344@qq.com>
2018-04-11 23:20:00 +0800

    vue_video init

---
+++ my-project/src/components/List.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="list">
+    <h1>{{ msg }}</h1>
+    <h2>list</h2>
+    <router-link to="/play">
+      <div class="toPlay">
+        {{$route.params.id}}
+      </div>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  created() {
+    console.log('1');
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="less">
+  .list{
+    border: 1px solid green;
+    h2{
+      color: blue;
+    }
+    .toPlay{
+      width:375px;
+      height: 375px;
+      border:1px solid yellow;
+    }
+  }
+</style>
---
+++ my-project/src/components/Play.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="play">
+    <h1>{{ msg }}</h1>
+    <h2>play</h2>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="less">
+  .play{
+    border: 1px solid red;
+    h1{
+      color: yellow;
+    }
+  }
+</style>
